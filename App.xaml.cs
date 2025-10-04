882e7d6e45436973aacbbb8a4de251ad7a919a3b
dimi7 <dimi7rof@gmail.com>
2025-10-04 13:51:12 +0300

    refactor: cleanup

--- App.xaml.cs
+++ App.xaml.cs
@@ -1,8 +1,4 @@
-﻿using System.Configuration;
-using System.Data;
-using System.Windows;
-
-namespace SubtitleRenamerWpf;
+﻿namespace SubtitleRenamerWpf;
 
 /// <summary>
 /// Interaction logic for App.xaml
